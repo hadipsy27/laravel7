bb9aee4512d432e3fdf6cf54dfcf045890ffcd8c
hadipsy27 <hadipsy27@gmail.com>
2020-08-15 12:30:55 +0700

    memperkenalkan tabel ke model dan page 404

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,7 @@ class PostController extends Controller
 {
     public function show($slug)
     {
+        // fistOrFail() suatu method jika data tidak ada maka akan di print 404
         $post = Post::where('slug', $slug)->firstOrFail();
         // dd($post); // http://127.0.0.1:8000/posts/my-first-post -> my-first-post merupakan data slug di tabel posts
 

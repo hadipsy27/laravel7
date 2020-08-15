b1c7d481c004b8f2a9779230051cda0da4eec6ac
hadipsy27 <hadipsy27@gmail.com>
2020-08-15 12:29:42 +0700

    memperkenalkan table ke model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->firstOrFail();
         // dd($post); // http://127.0.0.1:8000/posts/my-first-post -> my-first-post merupakan data slug di tabel posts
 
         return view('posts.show',compact('post'));
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,20 +3,22 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item{{request()->is('/') ? ' active' : ''}}">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item{{request()->is('contact') ? ' active' : ''}}">
-        <a class="nav-link" href="/contact">Contact</a>
-      </li>
-      <li class="nav-item{{request()->is('about') ? ' active' : ''}}">
-        <a class="nav-link" href="/about">About</a>
-      </li>
-      <li class="nav-item {{request()->is('login') ? ' active' : ''}}">
-        <a class="nav-link" href="/login">Login</a>
-      </li>
-    </ul>
-  </div>
+<div class="container">
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav nav-pills">
+        <li class="nav-item{{request()->is('/') ? ' active' : ''}}">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item{{request()->is('contact') ? ' active' : ''}}">
+            <a class="nav-link" href="/contact">Contact</a>
+        </li>
+        <li class="nav-item{{request()->is('about') ? ' active' : ''}}">
+            <a class="nav-link" href="/about">About</a>
+        </li>
+        <li class="nav-item {{request()->is('login') ? ' active' : ''}}">
+            <a class="nav-link" href="/login">Login</a>
+        </li>
+        </ul>
+    </div>
+</div>
 </nav>

d75b13be2a3ba6ad99f2683f9bca2855657c5b91
hadipsy27 <hadipsy27@gmail.com>
2020-08-15 09:49:33 +0700

    show method slug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,9 @@ class PostController extends Controller
     public function show($slug)
     {
         $post = \DB::table('posts')->where('slug', $slug)->first();
-        dd($post); // http://127.0.0.1:8000/posts/my-first-post -> my-first-post merupakan data slug di tabel posts
+        // dd($post); // http://127.0.0.1:8000/posts/my-first-post -> my-first-post merupakan data slug di tabel posts
 
-        return view('posts.show',compact('slug'));
+        return view('posts.show',compact('post'));
 
 
     }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
-@section('title', $slug)
+@section('title', $post->title)
 @section('content')
     <div class="container">
-        <p> {{ $slug }} </p>
+        <!-- <p> {{ $post->slug }} </p> -->
+        <h1> {{ $post->title }} </h1>
+        <p> {{ $post->body }} </p>
     </div>
 @endsection

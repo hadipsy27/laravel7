3664c665cec6dbef7cc3fd228112c87abed5c9c5
hadipsy27 <hadipsy27@gmail.com>
2020-08-15 09:09:49 +0700

    slug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,11 @@ class PostController extends Controller
 {
     public function show($slug)
     {
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+        dd($post); // http://127.0.0.1:8000/posts/my-first-post -> my-first-post merupakan data slug di tabel posts
+
         return view('posts.show',compact('slug'));
-        // http://127.0.0.1:8000/posts/this-is-my-post
+
+
     }
 }

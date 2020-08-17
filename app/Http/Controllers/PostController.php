453d6b949fb2cd9e955dd217f16cd078f50fea31
hadipsy27 <hadipsy27@gmail.com>
2020-08-17 11:48:54 +0700

    Paginate with number

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        // return Post::get(); -> menampilkan semua data dalam tabel post
+        // return Post::get(['title','slug']); // menampilkan title,slug dalam tabel post
+        // panggil di browser -> 127.0.0.1:8000/posts/ 
+
+        $post = Post::paginate(2);
+        return view('posts.index',['posts' => $post]);
+
+    }
     public function show(Post $post)
     {
         // http://127.0.0.1:8000/posts/my-first-post -> my-first-post merupakan data slug di tabel posts
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <h4>All Post</h4>  
+        {{-- .card>.card-header+.card-body --}}
+        @foreach ($posts as $post)
+            
+        <div class="card  mb-3">
+          <div class="card-header">
+            {{ $post->title }}
+          </div>
+          <div class="card-body">
+            <div>{{ Str::limit($post->body, 100, '.') }}</div>
+
+            <a href="">Read more</a>
+          </div>
+        </div>
+
+        @endforeach
+        {{ $posts->links() }}
+    </div>  
+  </div>
+</div>
+@endsection

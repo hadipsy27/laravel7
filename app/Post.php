fd5f19edbf510f540d0d48c5980f8101ab83b950
hadipsy27 <hadipsy27@gmail.com>
2020-08-17 10:54:25 +0700

    eloquent / tinker

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-
+  public function scoopeLatesFirst(){
+    // php artisan tinker, lalu
+    // Post::latest->first();
+    //--------------------------
+    return $this->latest()->first();
+  }
 }

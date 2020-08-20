d8e41ae191458a2fc007be3b774f417564d02f92
hadipsy27 <hadipsy27@gmail.com>
2020-08-20 10:03:09 +0700

    guarded pada post model

--- app/Post.php
+++ app/Post.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
   // menentukan form mana yang boleh diisi
-  protected $fillable = ['title', 'slug', 'body'];
+  // protected $fillable = ['title', 'slug', 'body'];
+
+  // guarded di pakai jika data yang di isi mempunyai scope hanya pada satu sisi(admin)
+  // karena guarded memiliki keamanan yang lemah, yg bisa menambah id sesuka hait pada field database
+  protected $guarded = [];
 }

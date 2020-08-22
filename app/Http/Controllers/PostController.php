56683666aa4142f3d07875c18a79068efb0504e6
hadipsy27 <hadipsy27@gmail.com>
2020-08-22 10:42:02 +0700

    validate 2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -12,7 +12,6 @@ class PostController extends Controller
         return view('posts.index',[
             'posts' => Post::latest()->paginate(6),
         ]);
-
     }
 
     public function show(Post $post)
@@ -25,9 +24,9 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store()
+    public function store(PostRequest $request)
     {
-        $attr = $this->validateRequest();
+        $attr = $request->all();
         $attr['slug'] = \Str::slug(request('title'));
         Post::create($attr);
         session()->flash('success','The post was created');
@@ -39,20 +38,12 @@ class PostController extends Controller
         return view('posts.edit',compact('post'));
     }
 
-    public function update(Post $post)
+    public function update(PostRequest $request, Post $post)
     {
-        $attr = $this->validateRequest();
+        $attr = $request->all();
         $post->update($attr);
         session()->flash('success', 'The post was updated');
         return redirect('posts');
-    }
-
-    public function validateRequest()
-    {
-        return request()->validate([
-            'title'     => 'required|min:3',
-            'body'      => 'required',
-        ]);
     }
 
 }

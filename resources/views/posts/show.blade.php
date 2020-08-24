e98b690ce4f7b157b0c740f9c08d2a8588e68b3f
hadipsy27 <hadipsy27@gmail.com>
2020-08-24 11:49:52 +0700

    destroy with modal

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,13 +6,41 @@
         <!-- <p> {{ $post->slug }} </p> -->
         <h1> {{ $post->title }} </h1>
         <p> {{ $post->body }} </p>
-
         <div>
-            <form action="/posts/{{ $post->slug }}/delete" method="post">
-                @csrf
-                @method('delete')
-                <button class="btn btn-link btn-sm text-danger p-0" type="submit">Delete</button>
-            </form>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-link text-danger btn-sm p-0" data-toggle="modal" data-target="#exampleModal">
+            Destroy
+        </button>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Apakah anda yakin menghapusnya? </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                    <div class="mt-1">
+                        <div>{{ $post->title}}</div>
+                        <small>
+                            Published: {{ $post->created_at->format('d F, y')}}
+                        </small>
+                    </div>
+                    <form action="/posts/{{ $post->slug }}/delete" method="post">
+                        @csrf
+                        @method('delete')
+                        <div class="d-flex mt-4">
+                            <button class="btn btn-danger mr-2" type="submit">Ya</button>
+                            <button type="button" class="btn btn-success" data-dismiss="modal">Tidak</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            </div>
+        </div>
         </div>
     </div>
 @endsection
